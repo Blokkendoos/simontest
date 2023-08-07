420f96f8c61358127cf543eb79c27446bac288ac
blokkendoos <jvo@chaosgeordend.nl>
2023-08-07 21:46:18 +0200

    Update for ROS noetic,
    Python3, relative imports, PEP8 (flake8)

--- src/simontest/foo.py
+++ src/simontest/foo.py
@@ -1,11 +1,13 @@
-print "  importing bar_fn, baz_fn from simon_pythonsubpackage1"
-from simon_pythonsubpackage1 import bar_fn, baz_fn
-print "  imported bar_fn, baz_fn from simon_pythonsubpackage1"
+print("  importing bar_fn, baz_fn from simon_pythonsubpackage1")
+from . simon_pythonsubpackage1 import bar_fn, baz_fn
+print("  imported bar_fn, baz_fn from simon_pythonsubpackage1")
+
 
 def main():
-	print " executing main() in foo.py"
-	bar_fn()
-	baz_fn()
+    print(" executing main() in foo.py")
+    bar_fn()
+    baz_fn()
+
 
 if __name__ == '__main__':
     main()
